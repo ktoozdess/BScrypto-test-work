1efb80859ade9d9eb510b7ef1a4ceacb2cea2a98
Ktoozdess <tigerfpvteam@gmail.com>
2022-05-19 23:29:36 +0500

    in the past 14 days

--- js/main.js
+++ js/main.js
@@ -4,10 +4,14 @@
     const result = document.querySelector('#result');
     const selectfirst = document.querySelector('#select-1');
     const selectsecond = document.querySelector('#select-2');
-
+    const BTC14d = document.querySelector('#BTC14d')
+    const ETH14d = document.querySelector('#ETH14d')
+    const USD14d = document.querySelector('#USD14d')
     const rates = {};
-        
-    fetch('https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd')
+    
+
+
+    fetch('https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=price_change_14d_desc&page=1&sparkline=false&price_change_percentage=14d')
     .then(response => response.json())
     .then(data => {
        
@@ -17,15 +21,23 @@
         // console.log(data);
         console.log(rates);
 
+        BTC14dValue = rates.BTCUSD.price_change_percentage_14d_in_currency;
+        BTC14d.innerHTML = (BTC14dValue).toFixed(1) + '%';
+
+        ETH14dValue = rates.ETHUSD.price_change_percentage_14d_in_currency;
+        ETH14d.innerHTML = (ETH14dValue).toFixed(1) + '%';
+
+       
     
     })
-    fetch('https://api.coingecko.com/api/v3/coins/markets?vs_currency=rub')
+    fetch('https://api.coingecko.com/api/v3/coins/markets?vs_currency=rub&order=price_change_14d_desc&page=1&sparkline=false&price_change_percentage=14d')
     .then(response => response.json())
     .then(data => {
        
        
         rates.USDRUB = data['3']
-      
+        USD14dValue = rates.USDRUB.price_change_percentage_14d_in_currency;
+        USD14d.innerHTML = (USD14dValue).toFixed(1) + '%';
     })
 
     input.oninput = convert;
